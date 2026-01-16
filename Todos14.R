1500d57834d79d3a4828b96ec271aae8b2ec4170
Arya Yousefi <aryayousefi1815@gmail.com>
2026-01-16 20:37:07 +0100

    Add Todos14

---
+++ Todos14.R
@@ -0,0 +1,15 @@
+2+2
+x <- 10
+x * 5
+numbers <- c(4, 5, 8, 11)
+sum(numbers)
+x <- rnorm(100, mean = 0, sd = 2)
+plot(x)
+hist(x)
+start_year <- 2023
+birth_year <- 2002
+years_at_college <- 2026 - start_year
+years_alive <- 2026 - birth_year
+percentage <- (years_at_college / years_alive) * 100
+percentage
+
